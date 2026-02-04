b15764dbbe8db41e381894a9bee1dac9052268fa
seraicerd-star <seraicerd@gmail.com>
2026-02-04 21:11:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const MODEL_MAPPING = {
   'gpt-4-turbo': 'moonshotai/kimi-k2-instruct-0905',
   'gpt-4o': 'deepseek-ai/deepseek-v3.1',
   'claude-3-opus': 'openai/gpt-oss-120b',
-  'claude-3-sonnet': 'zai-org/GLM-4.7',
+  'claude-3-sonnet': 'z-ai/glm4.7',
   'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking' 
 };
 
