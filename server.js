9947807f485f22ffec95d935b8c3796f1abe8380
seraicerd-star <seraicerd@gmail.com>
2026-02-06 15:52:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/v1/chat/completions', async (req, res) => {
     const nimRequest = {
       model: nimModel,
       messages: messages,
-      temperature: temperature || 1.05,
+      temperature: temperature || 1.5,
       max_tokens: max_tokens || 9024,
       extra_body: ENABLE_THINKING_MODE ? { chat_template_kwargs: { thinking: true } } : undefined,
       stream: stream || false
